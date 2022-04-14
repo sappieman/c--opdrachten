e6e641eb79f15182b639608da46083ae0663b1c2
bas klaichid <basklaichid@gmail.com>
2022-04-14 10:22:41 +0200

    second commit'

---
+++ Namespaces/Namespaces RIGHT/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespaces_2
+{
+    internal class Class1
+    {
+        private int koek;
+
+        public void Feed(int amount)
+        {
+            koek -= amount;
+            Console.WriteLine(koek);
+        }
+    }
+
+
+}
---
+++ Namespaces/Namespaces RIGHT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Namespaces
+{
+    internal class Program : Namespaces_2. Class1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
